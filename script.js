6221ac4c948be63e62ea54c4938a8ef79f934f49
Yoona8 <elena243v@gmail.com>
2021-12-14 11:51:47 +0300

    Decouple the logic, add logger and descriptions

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
-/************************************************************************************************
- *                                                                                              *
- *                              VARIABLES DECLARATION                                           *
- *                                                                                              *
- ************************************************************************************************/
-var adIsViewable = true,
-  viewabilityTime = 0,
-  adElement = document.getElementById("ad");
+const MS_IN_SEC = 1000;
+
+let adIsViewable = true;
+let viewabilityTime = 0;
+let logFrequency = 500;
+let logCustom = null;
+let logInterval = null;
+const adElement = document.getElementById("ad");
+
+/**
+ * Description of the custom logging function
+ * 
+ * @callback ILog
+ * @param {boolean} adIsVisible Ad is visible or not
+ * @param {number} viewabilityTime Viewability time of the ad (in sec) 
+ */
+
+/**
+ * Initializes the logger
+ * 
+ * @param {object} config Logger configuration
+ * @param {number} config.frequency Logging frequency (in ms)
+ * @param {ILog} config.customLog Custom logging function
+ */
+window.setLogging = function (config) {
+  const {frequency = 500, customLog} = config;
+
+  logFrequency = frequency;
+  logCustom = customLog;
+  resetLog();
+};
 
 /**
 * Logs the viewability values in the console
 *
 * @override
 */
-window.log = function () {
-  console.log("Ad is viewable: ", adIsViewable, "\nViewability time of the ad in sec:", viewabilityTime);
+const log = function () {
+  if (logCustom) {
+    logCustom(adIsViewable, Math.ceil(viewabilityTime));
+    return;
+  }
+
+  console.log("Ad is viewable: ", adIsViewable, "\nViewability time of the ad in sec:", Math.ceil(viewabilityTime));
 };
 
-/************************************************************************************************
-*                                                                                              *
-*                              YOUR IMPLEMENTATION                                             *
-*                                                                                              *
-************************************************************************************************/
+const resetLog = function () {
+  adIsViewable = true;
+
+  logInterval = setInterval(function () {
+    viewabilityTime += logFrequency / MS_IN_SEC;
+    log();
+  }, logFrequency);
+};
